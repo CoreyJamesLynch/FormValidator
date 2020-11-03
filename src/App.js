b3c2219a08153afa78837e76fc729ef9806ff5ee
bearinawolfpack <bearinawolfpack@gmail.com>
2020-11-03 17:46:27 -0500

    Create form in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    password2: '',
+  };
+
+  clickHandler = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { username } = this.state;
+    const { email } = this.state;
+    const { password } = this.state;
+    const { password2 } = this.state;
+    return (
+      <div className="container">
+        <form id="form" className="form">
+          <h2>Register With Us</h2>
+          <label htmlFor="username">
+            Username
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter username"
+              name="username"
+              value={username}
+              onChange={this.clickHandler}
+            />
+          </label>
+          <small>Error Message</small>
+
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email"
+              name="email"
+              value={email}
+              onChange={this.clickHandler}
+            />
+          </label>
+          <small>Error Message</small>
+
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter password"
+              name="password"
+              value={password}
+              onChange={this.clickHandler}
+            />
+          </label>
+          <small>Error Message</small>
+
+          <label htmlFor="password2">
+            Confirm Password
+            <input
+              type="password"
+              id="password2"
+              placeholder="Confirm password"
+              name="password2"
+              value={password2}
+              onChange={this.clickHandler}
+            />
+          </label>
+          <small>Error Message</small>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
